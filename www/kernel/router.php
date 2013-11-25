0ac69152916ea22f5bf0bc8a3608666c48357c71
ruslan <stelss1986@gmail.com>
2013-11-26 01:28:58 +0200

    Routes

--- www/kernel/router.php
+++ www/kernel/router.php
@@ -4,15 +4,117 @@ class Router
   {
 
   // Хранит конфигурацию маршрутов.
-  private $routes;
+  private static  $routes = array();
   private $fileExt = '';
   
   function __construct($routesPath = 'config-router.php')
     {
     // Получаем конфигурацию из файла.
-    $this->routes = include($routesPath);
+    $this->includeRotes();
     }
 
+  function includeRotes()
+    {
+    $dir_handler = opendir('modules');
+    while ($dir = readdir($dir_handler))
+      {
+      if ((is_dir("modules/$dir")) &&
+                    ($dir != '.') &&
+                    ($dir != '..') &&
+                    ($dir != 'CVS') &&
+                    (file_exists ("modules/$dir/router.php")))
+        {
+        include_once ("modules/$dir/router.php");
+        }
+      }
+    closedir($dir_handler);
+    }
+    
+  public static function add($template, $params)
+    {
+    self::$routes[] = array($template, $params);
+    }
+    
+  function runModuleRoutes()
+    {
+    $fullURL = parse_url($_SERVER["REQUEST_URI"]);
+    
+    foreach(self::$routes as $route)
+      {
+      $mod = $route[1]['controller'];
+      $action = $route[1]['action'];
+      $pattern = str_replace("/", "\/", $route[0]);  
+      $route_vars =array();
+      $parameters = array();
+      
+      if(preg_match_all('/<(\w+):?(.*?)?>/',$pattern,$matches))
+        {
+        $tokens=array_combine($matches[1],$matches[2]);
+
+        foreach($tokens as $name=>$value)
+          {
+          if($value==='')
+            $value='[^\/]+';
+          
+          $tr["<$name:$value>"]="(?P<$name>$value)";
+          $route_vars[] = $name;
+          }
+        $routePattern = strtr($pattern, $tr);
+        }
+      else
+        {
+        $routePattern = $pattern;
+        }
+        
+      
+      
+      preg_match("/^$routePattern$/u", $fullURL['path'], $matches);
+      
+      if($matches)
+        {
+        if($matches['controller'])
+          {
+          $mod = $matches['controller'];
+          unset($matches['controller']);
+          }
+        if($matches['action'])
+          {
+          $action = $matches['action'];
+          unset($matches['action']);
+          }
+        
+        if(!file_exists("modules/$mod/index.php"))
+          trigger_error ("Module '".$mod."' or module controler not exist", E_USER_ERROR);
+
+        
+        foreach($route_vars as $var)
+          {
+          $parameters[$var] = $matches[$var];
+          }
+        
+        
+        include_once "modules/$mod/index.php";
+        //$module = new $mod();
+        $module = new Index($mod);
+
+        global $mod_controller;
+        $mod_controller = $module;
+
+        $module->input_vars = array_merge($parameters, $module->input_vars);
+
+        if(empty($_REQUEST['type']))
+          $module->type = 'user';
+        else  
+          $module->type = ($_REQUEST['type'] == 'user' || $_REQUEST['type'] == 'admin'  || $_REQUEST['type'] == 'ajax') ? $_REQUEST['type'] : 'user';
+
+        $module->action($action);
+
+        exit; 
+        }
+      }
+
+    }
+    
   function curPageURL() {
     $pageURL = 'http';
     if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
@@ -47,6 +149,8 @@ class Router
 
   function run()
     {
+    $this->runModuleRoutes();
+    
     $fullURL = parse_url($_SERVER["REQUEST_URI"]);
     
     if($fullURL['path'] == '/' || $fullURL['path'] == '' || $fullURL['path'] == '/index.php')
---
+++ www/modules/blocks/router.php
@@ -0,0 +1,4 @@
+<?php
+Router::add("/users/", array('controller'=>'blocks', 'action'=>'blocks_list'));
+//Router::add("/users/<id:\d+>/<worr:\w+>", array('controller'=>'blocks', 'action'=>'blocks_list'));
+?>
