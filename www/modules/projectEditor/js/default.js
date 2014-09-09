8f433884b7fb5d60daa1303a56d4d90b6543a94e
Ruslan <stelss1986@gmail.com>
2014-09-09 18:18:03 +0300

    project editor

--- www/modules/projectEditor/js/default.js
+++ www/modules/projectEditor/js/default.js
@@ -1,37 +1,42 @@
 var editor =null;
 var delay;
+var tabCounter = 2, tabs;
 var mime = [];
 mime['ext-php'] = "application/x-httpd-php";
 mime['ext-css'] = "text/css";
 mime['ext-js']  = "text/javascript";
+//mime['ext-tpl'] = "text/x-smarty";
 mime['ext-tpl'] = "application/x-httpd-php";
 mime['ext-html'] = "text/html";
 
 
-function setCodeStyle(mime)
+function setCodeStyle(mime, element)
     {
-    alert(mime);  
-    $('.CodeMirror').remove();  
+    //$('.CodeMirror').remove();  
     
 //    CodeMirror.commands.autocomplete = function(cm) {
 //        CodeMirror.simpleHint(cm, CodeMirror.javascriptHint);
 //      }
-      
-    editor = CodeMirror.fromTextArea(document.getElementById("codeEdit"), {
+  
+    editor = CodeMirror.fromTextArea(document.getElementById(element), {
         lineNumbers: true,
         matchBrackets: true,
         mode: {name: mime, globalVars: true},
         indentUnit: 4,
         indentWithTabs: true,
         enterMode: "keep",
+        keyMap: "sublime",
+        autoCloseBrackets: true,
         tabMode: "shift",
+        //theme: "monokai",
         extraKeys: {"Ctrl-Space": "autocomplete"},
-        onChange: function() {
-          clearTimeout(delay);
-          delay = setTimeout(updatePreview, 300);
-        }
+//        onChange: function() {
+//          clearTimeout(delay);
+//          delay = setTimeout(updatePreview, 300);
+//        }
         });
     }
+    
  function updatePreview() 
   {
   var previewFrame = document.getElementById('preview');
@@ -40,26 +45,58 @@ function setCodeStyle(mime)
   preview.write(editor.getValue());
   preview.close();
   }
+  
+  
+  
+  function addTab(title, content, mime) 
+    {
+    tabCounter++;
+    var 
+      id = "tabs-" + tabCounter,
+      li = "<li><a href='#"+id+"'>"+title+"</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
+      tabHeight = $('.editor-text').height() - 50;
+
+    tabs.find( ".ui-tabs-nav" ).append( li );
+    tabs.append( "<div id='" + id + "' style='height: "+tabHeight+"px; '><textarea id='codeEdit-"+tabCounter+"' name='codeEdit-"+tabCounter+"'></textarea></div>" );
+    tabs.tabs( "refresh" );
+    tabs.tabs('select', $(".ui-tabs-nav").children().size() - 1);
+    
+    $('#codeEdit-'+tabCounter).val(content);
+    setCodeStyle(mime, 'codeEdit-'+tabCounter);
+ 
+    }
       
 $(document).ready(function(){
+  
+  $('.editor-text').height($(window).height() - $('.editor-menu').height() - 25);
+  
+  tabs = $( "#tabs" ).tabs();
+  
+  // close icon: removing the tab on click
+  tabs.delegate( "span.ui-icon-close", "click", function() {
+    var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+    $( "#" + panelId ).remove();
+    tabs.tabs( "refresh" );
+  });
   
   $('.pft-file a').click(function(){
     var parentClasses = $(this).parent().attr('class');
     var ext = parentClasses.replace("pft-file ","");
+    var label = $(this).html();
     
 //    $('#codeEdit').load($(this).attr('href'), function(){
 //      setCodeStyle(mime[ext]);
 //    });
     
     $.post($(this).attr('href'), function(data) {
-      $('#codeEdit').val(data);
-      setCodeStyle(mime[ext]);
+      
+      addTab(label, data, mime[ext]);
     });
     
     return false;
   });
   
-  setCodeStyle('html');
-  setTimeout(updatePreview, 300);
+  //setCodeStyle('html');
+  //setTimeout(updatePreview, 300);
 });
 
--- www/modules/projectEditor/views/default/viewEditor.tpl
+++ www/modules/projectEditor/views/default/viewEditor.tpl
@@ -9,17 +9,31 @@
     <meta charset="utf-8"/>
 
     <link rel=stylesheet href="/modules/projectEditor/js/lib/codemirror.css">
+    <link rel=stylesheet href="/modules/projectEditor/js/theme/monokai.css">
     <link rel=stylesheet href="/modules/projectEditor/js/doc/docs.css">
+    <link rel=stylesheet href="/modules/projectEditor/js/addon/hint/show-hint.css">
+      
     <script src="/modules/projectEditor/js/lib/codemirror.js"></script>
+    <script src="/modules/projectEditor/js/mode/clike/clike.js"></script>
     <script src="/modules/projectEditor/js/mode/xml/xml.js"></script>
+    <script src="/modules/projectEditor/js/mode/php/php.js"></script>
     <script src="/modules/projectEditor/js/mode/javascript/javascript.js"></script>
     <script src="/modules/projectEditor/js/mode/css/css.js"></script>
     <script src="/modules/projectEditor/js/mode/htmlmixed/htmlmixed.js"></script>
+    <script src="/modules/projectEditor/js/mode/smarty/smarty.js"></script>
+    <script src="/modules/projectEditor/js/mode/smartymixed/smartymixed.js"></script>
+    <script src="/modules/projectEditor/js/mode/sql/sql.js"></script>
+    
     <script src="/modules/projectEditor/js/addon/edit/matchbrackets.js"></script>
+    <script src="/modules/projectEditor/js/keymap/sublime.js"></script>
     
     {*HINTS autocomplate*}
     <script src="/modules/projectEditor/js/addon/hint/show-hint.js"></script>
     <script src="/modules/projectEditor/js/addon/hint/javascript-hint.js"></script>
+    <script src="/modules/projectEditor/js/addon/hint/css-hint.js"></script>
+    <script src="/modules/projectEditor/js/addon/hint/html-hint.js"></script>
+    <script src="/modules/projectEditor/js/addon/hint/simple-hint.js"></script>
+    <script src="/modules/projectEditor/js/addon/hint/sql-hint.js"></script>
     
 
 {*    <script src="/modules/projectEditor/js/doc/activebookmark.js"></script>*}
@@ -41,33 +55,29 @@
 
 	<body>
 	
-    <table height="100%" width="100%" style="border: 1px solid #000; min-height: 100%">
+    <table height="100%" width="100%"  style="border: 1px solid #000; max-height: 100%">
       <tr>
-        <td colspan="2" height="35">
+        <td colspan="2" height="35" class="editor-menu">
           menu
         </td>
       </tr>
     
       <tr>
-        <td height="500px" width="300" style="border: 1px solid #000;" valign="top">
-          <div style="width: 300px; height: 500px; overflow: scroll;">
+        <td height="100%" width="300" style="border: 1px solid #000;" valign="top">
+          <div style="width: 300px; height: 100%; overflow: scroll;">
             {$phpTree}
           </div>
         </td>
-        <td height="500px" width="*" style="border: 1px solid #000;" valign="top">
-                    
-           
-                    <textarea id='codeEdit' name='codeEdit'>
-                    </textarea>
-               
-        </td>
-
-      </tr>
-      <tr>
-        <td colspan="2">
-          <iframe id='preview' style="width: 99%; height: 290px;"></iframe>
+        <td height="100%" width="*" class="editor-text" style="border: 1px solid #000; max-height: 100%" valign="top">
+         
+          <div id="tabs">
+            <ul>
+            </ul>
+          </div>
+          
         </td>
       </tr>
+      
     </table>
 		
 	</body>
